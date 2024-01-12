c2d76b90909fe4bf85b0dd3e595096c8c8ff6f26
BarVinizky <barr98@gmail.com>
2024-01-12 13:29:38 +0200

    q1-5 Bar version

---
+++ q2.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="covid_db",
+        port='3307',
+    )
+    cursor = mydb.cursor()
+    cursor.execute("""
+        SELECT date, new_cases
+        FROM covid_deaths
+        WHERE location = 'south america' AND new_cases > 150000
+        ORDER BY new_cases ASC
+        
+    """)
+    print(', '.join(str(row) for row in cursor.fetchall()))
---
+++ q4.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="covid_db",
+        port='3307',
+    )
+    cursor = mydb.cursor()
+    cursor.execute("""
+        SELECT DISTINCT date, new_cases
+        FROM covid_deaths
+        WHERE location = 'Israel'
+        ORDER BY new_cases DESC
+        LIMIT 20
+
+    """)
+    print(', '.join(str(row) for row in cursor.fetchall()))
